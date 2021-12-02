567051cc64430b58f0260d69aa7a8b6a6eeebda2
Ragub0413 <ragubangelica13@gmail.com>
2021-12-03 00:33:25 +0800

    env and hidding db

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+import {} from 'dotenv/config'
+
+
+
 import userRouter from './routes/user.js';
 const app = express();
 
@@ -13,13 +18,15 @@ app.use(cors());
 
 app.use('/user',userRouter);
 
-const CONNECTION_DB = 'mongodb+srv://SDMC:SantosDelCarmenMedicalClinicProgram@rest.kojhd.mongodb.net/SDMC?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 
+const CONNECTION_DB = process.env.ATLAS_URI;
 mongoose.connect(CONNECTION_DB, {useNewUrlParser:true, useUnifiedTopology:true})
-    .then(()=>app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
+     .then(()=>app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
+
+
 
 //mongoose.set('useFindAndModify', false);
 //app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`))
